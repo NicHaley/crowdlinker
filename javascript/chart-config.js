38ab994bd59de2e2c05418323df04bfca7bfefce
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-30 11:30:14 -0400

    Served active player data as JSON, repositioned top nav

---
+++ javascript/chart-config.js
@@ -0,0 +1,27 @@
+$(document).on('ready', function(){
+
+	Chart.defaults.global.responsive = true;
+
+	// var data = {
+	//     labels: ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
+	//     datasets: [
+	//         {
+	//             label: "My First dataset",
+	//             fillColor: "rgba(220,220,220,0.2)",
+	//             strokeColor: "rgba(220,220,220,1)",
+	//             pointColor: "rgba(220,220,220,1)",
+	//             pointStrokeColor: "#fff",
+	//             pointHighlightFill: "#fff",
+	//             pointHighlightStroke: "rgba(220,220,220,1)",
+	//             data: [40, 25, 30, 50, 30, 28, 40, 20]
+	//         }
+	//     ]
+	// };
+
+	// var ctx = $("#myChart").get(0).getContext("2d");
+	// // This will get the first returned node in the jQuery collection.
+	// var myLineChart = new Chart(ctx).Line(data, {
+	// 	scaleShowVerticalLines: false,
+	// 	bezierCurve: false
+	// });
+});
