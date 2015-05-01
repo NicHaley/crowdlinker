b274097d212d9e60605c88622066d8fdf06f81a8
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-01 15:14:10 -0400

    Rejigged progressbar to work with mustache tempaltes

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,6 @@
 $(document).on('ready', function(){
 
-	var newPlayers = new ProgressBar.Circle('#newPlayers', {
-		color: '#ea4123',
-		trailColor: "#eaeaea",
-		strokeWidth: 10,
-		duration: 2000,
-		easing: 'easeInOut'
-	});
 
-	var revisited = new ProgressBar.Circle('#revisited', {
-		color: '#ea4123',
-		trailColor: "#eaeaea",
-		strokeWidth: 10,
-		duration: 2000,
-		easing: 'easeInOut'
-	});
-
-	var commented = new ProgressBar.Circle('#commented', {
-		color: '#ea4123',
-		trailColor: "#eaeaea",
-		strokeWidth: 10,
-		duration: 2000,
-		easing: 'easeInOut'
-	});
-
-	var activePlayers = new ProgressBar.Circle('#activePlayersPB', {
-		color: '#ea4123',
-		trailColor: "#eaeaea",
-		strokeWidth: 10,
-		duration: 2000,
-		easing: 'easeInOut'
-	});
 
 	$.ajax({
 		type: "GET",
@@ -43,15 +13,19 @@ $(document).on('ready', function(){
 		success:function(data){
 			var playerListTemplate = $('#playerListTemplate').html();
 			var statsTemplate = $('#statsTemplate').html();
+			var playerStatsTemplate = $('#playerStatsTemplate').html();
 
 			Mustache.parse(playerListTemplate);
 			Mustache.parse(statsTemplate);
+			Mustache.parse(playerStatsTemplate);
 
 			var renderedPLT = Mustache.render(playerListTemplate, data);
 			var renderedST = Mustache.render(statsTemplate, data.stats);
+			var renderedPST = Mustache.render(playerStatsTemplate, data.playerStats);
 
 			$('#recentPlayers').html(renderedPLT);
 			$('#stats').html(renderedST);
+			$('#playerStats').html(renderedPST);
 
 			var chart1 = $("#activePlayers").get(0).getContext("2d");
 			var chart2 = $("#activeTournaments").get(0).getContext("2d");
@@ -70,10 +44,43 @@ $(document).on('ready', function(){
 		}
 	});
 
-	function progressBarAnim(playerStats){
-		newPlayers.animate(playerStats.newPlayers/100);
-		revisited.animate(playerStats.revisited/100);
-		commented.animate(playerStats.commented/100);
-		activePlayers.animate(playerStats.activePlayers/100);		
-	}
+		function progressBarAnim(playerStats){
+
+			var newPlayers = new ProgressBar.Circle('#newPlayers', {
+				color: '#56606e',
+				trailColor: "#d2d4d8",
+				strokeWidth: 10,
+				duration: 2000,
+				easing: 'easeInOut'
+			});
+
+			var revisited = new ProgressBar.Circle('#revisited', {
+				color: '#91c46b',
+				trailColor: "#d2d4d8",
+				strokeWidth: 10,
+				duration: 2000,
+				easing: 'easeInOut'
+			});
+
+			var commented = new ProgressBar.Circle('#commented', {
+				color: '#56606e',
+				trailColor: "#d2d4d8",
+				strokeWidth: 10,
+				duration: 2000,
+				easing: 'easeInOut'
+			});
+
+			var activePlayers = new ProgressBar.Circle('#activePlayersPB', {
+				color: '#91c46b',
+				trailColor: "#d2d4d8",
+				strokeWidth: 10,
+				duration: 2000,
+				easing: 'easeInOut'
+			});
+
+			newPlayers.animate(playerStats.newPlayers/100);
+			revisited.animate(playerStats.revisited/100);
+			commented.animate(playerStats.commented/100);
+			activePlayers.animate(playerStats.activePlayers/100);		
+		}
 });
